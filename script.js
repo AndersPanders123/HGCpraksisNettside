d79360279276bd9cbf5d49ab3ca4c0209389c8f8
AndersPanders123 <123541105+AndersPanders123@users.noreply.github.com>
2023-04-20 22:53:31 +0200

    -

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const images = document.querySelectorAll(".slider img");
 const imag = document.getElementsByClassName("slider");
 let currentImageIndex = 0; //bilde nummer
-const imgList = ["pic/divios.png", "pic/karate.png", "pic/setupp.png"]; //bildene
+const imgList = ["pic/divios.png", "pic/karate.png", "pic/nigga.png"]; //bildene
 
 window.onload = handleImageChange(1); //når vinduet laster bruk denne funksjonen
 function startImageSlider() {
@@ -13,7 +13,7 @@ function startImageSlider() {
 startImageSlider(); //bare kjør dette
 
 function handleImageChange() {
-  document.querySelectorAll(".imageSpinner").forEach((element) => {
+  document.querySelectorAll("img").forEach((element) => {
     //før vi bytter bilde fjerner vi alle bildene fra før
     element.remove();
   });
@@ -23,26 +23,15 @@ function handleImageChange() {
   const item = document.createElement("img"); //lage nytt bilde element
   item.src = imgList[currentImageIndex]; //endre kilden til bilde
 
-  if (
-    document.getElementById("slider") != undefined ||
-    document.getElementById("slider") != null
-  ) {
-    document.getElementById("slider").appendChild(item); //legge til bilde
-  }
+  document.getElementById("slider").appendChild(item); //legge til bilde
 }
 
 const nextBtn = document.getElementById("nextBtn");
-
-if (nextBtn != null || nextBtn != undefined) {
-  nextBtn.addEventListener("click", function (e) {
-    nextBtn.addEventListener("click", handleImageChange(currentImageIndex + 1));
-  });
-}
+nextBtn.addEventListener("click", function (e) {
+  nextBtn.addEventListener("click", handleImageChange(currentImageIndex + 1));
+});
 
 const backBtn = document.getElementById("backBtn");
-
-if (backBtn != null || backBtn != undefined) {
-  backBtn.addEventListener("click", function (e) {
-    backBtn.addEventListener("click", handleImageChange(currentImageIndex - 1));
-  });
-}
+backBtn.addEventListener("click", function (e) {
+  backBtn.addEventListener("click", handleImageChange(currentImageIndex - 1));
+});
